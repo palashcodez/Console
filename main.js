29e8c54d389ab216dc74192f2605a30764ae1162
palashcodez <82249617+palashcodez@users.noreply.github.com>
2021-06-25 11:22:04 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ var first_name="Palash";
 var last_name="Bagkar";
 
 var full_name=first_name.concat(last_name);
+console.log(first_name);
+console.log(last_name);
+console.log(full_name);
 
-console.log(full_name)
 
 var a=10;
 var b=5;
 
 var add =a+b;
+console.log(a);
+console.log(b);
 console.log(add);
